227e01a7b6ca3b89cc28f17f9706af7dcbf33b88
MacBook Air <macbookair@ringgit.me>
2024-08-20 19:35:57 +0800

    Initial Commit

---
+++ Buckets/BucketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketsApp.swift
+//  Buckets
+//
+//  Created by Long Fong Yee on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buckets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Buckets
+//
+//  Created by Long Fong Yee on 20/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
