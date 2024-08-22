609c8d8094d3b7d3b4af2d7960f34b3f3aac8d2b
MacBook Air <macbookair@ringgit.me>
2024-08-22 12:04:35 +0800

    Some progress in KK

---
+++ Buckets/Views/ItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  ItemRow.swift
+//  Buckets
+//
+//  Created by Long Fong Yee on 21/08/2024.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemRow()
+}
